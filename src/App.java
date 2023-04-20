e65e3a695324386132b7e576c51e02a65458d0a7
jylmarcus <maplecow@gmail.com>
2023-04-20 14:18:16 +0800

    Added server skeleton

--- src/App.java
+++ src/App.java
@@ -1,5 +1,76 @@
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.EOFException;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        // 2 arguments
+        // 1 argument for file
+        // 1 argument for port server will start on
+        String fileName = args[0];
+        String port = args[1];
+
+        File cookieFile = new File(fileName); // assume fileName is full path
+
+        if (!cookieFile.exists()) {
+            System.out.println("Cookie file not found");
+            System.exit(0);
+        }
+
+        // slide 8 - establish server connection
+        ServerSocket ss = new ServerSocket(Integer.parseInt(port));
+        Socket socket = ss.accept();
+
+        // slide 9
+        try (InputStream is = socket.getInputStream()) {
+            BufferedInputStream bis = new BufferedInputStream(is);
+            DataInputStream dis = new DataInputStream(bis); // read up on this later
+
+            // store data sent over from client
+            String msgReceived = "";
+
+            try (OutputStream os = socket.getOutputStream()) {
+                BufferedOutputStream bos = new BufferedOutputStream(os);
+                DataOutputStream dos = new DataOutputStream(bos);
+
+                while (!msgReceived.equals("close")) {
+                    // slide 9 - receive message
+                    msgReceived = dis.readUTF();
+
+                    if (msgReceived.equals("get-cookie")) {
+                        // instantiate cookie.java
+                        // get a random cookie
+                        // send cookie using DOS
+                    }
+                    // slide 10 - send message
+
+                }
+
+                // close all output stream
+                dos.close();
+                bos.close();
+                os.close();
+
+            } catch (EOFException ex) {
+                ex.printStackTrace();
+            }
+
+            // close all input stream
+            dis.close();
+            bis.close();
+            is.close();
+
+        } catch (EOFException ex) {
+            socket.close();
+            ss.close();
+        }
     }
 }
---
+++ src/Cookie.java
@@ -0,0 +1,14 @@
+import java.util.List;
+
+public class Cookie {
+    List<String> cookies = null;
+
+    public void readCookieFile(String fileName) {
+
+    }
+
+    public String getRandomCookie() {
+
+        return "";
+    }
+}
